4485a14b94e7f1d016515c15cf974b287436136a
BrunoPostai <cechinelbruno@gmail.com>
2023-10-31 16:20:56 +0100

    product card for the store

--- horseTech/src/componentes/card/index.jsx
+++ horseTech/src/componentes/card/index.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
+import { Icon } from "@iconify/react";
 
 export function Card(props) {
   return (
-    <div>
-      { <img src={props.sela} alt="" /> }
-      <div className={styles.componente}>{props.name}</div>
+    <div className={styles.componente}>
+      <img
+        src="/public/sela1.png"
+        alt="imagem da sela"
+        className={styles.sela}
+      />
+      <div className={styles.container}>
+        <div className={styles.text}>
+          <h2>Titulo</h2>
+          <p>Descricao</p>
+        </div>
+        <div className={styles.arrow}>
+          <Icon icon="fluent:ios-arrow-24-filled" rotate={2} />
+        </div>
+      </div>
     </div>
   );
 }
-
-
--- horseTech/src/pages/loja.jsx
+++ horseTech/src/pages/loja.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../componentes/header/index";
+import { Card } from "../componentes/card/index";
 
 function Loja() {
   return (
     <div>
       <Header></Header>
+      <Card></Card>
       <h1>Pagina da loja</h1>
       <p>retornar ao <a href="/">menu</a></p>
     </div>
